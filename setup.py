a7b8eff1b1056bae68d88977ca76f91d02104c49
dmccloskey <dmccloskey87@gmail.com>
2015-08-05 09:12:54 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="sequencing_analysis",
+    version="0.1.a1",
+    description="Libraries for analysing sequencing data",
+    author="Douglas McCloskey",
+    author_email="dmccloskey87@gmail.com",
+    url="https://github.com/dmccloskey/sequencing_analysis",
+    packages=["sequencing_analysis"],
+    #classifiers=[
+    #'Development Status :: 5 - Production/Stable',
+    #'Environment :: Console',
+    #'Intended Audience :: Science/Research',
+    #'Operating System :: OS Independent',
+    #'Programming Language :: Python :: 3.4',
+    #'Programming Language :: Cython',
+    #'Programming Language :: Python :: Implementation :: CPython',
+    #'Topic :: Scientific/Engineering',
+    #'Topic :: Scientific/Engineering :: Bio-Informatics'
+    #],
+    platforms="GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7",
+    )
